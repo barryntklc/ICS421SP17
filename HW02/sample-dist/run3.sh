ea24a8792e56104ac9cd8fa2f3474cd71c3fcb0f
Chunmeista <barryn.chun@gmail.com>
2017-03-30 14:51:04 -1000

    removal of runtime items not source code

--- HW02/sample-dist/run3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sql="dist/lib/mysql-connector-java.jar"
-csv="dist/lib/opencsv-3.9.jar"
-
-#java -cp $sql:$csv -jar ./dist/SQLProc.jar ./sample/p3clustercfg ./sample/p3sqlfile 
-java -jar SQLProc.jar $1 $2
--- HW02/sample-dist/run4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sql="src/lib/mysql-connector-java.jar"
-csv="src/lib/opencsv-3.9.jar"
-
-java -jar SQLProc.jar $1 -csv $2
